91f63f72d73bb3e8e0583ddf41d7c08b5fdd7374
phamxuanloc0 <locpham2307@gmail.com>
2021-10-26 03:09:24 +0700

    add branch eureka_client

--- src/main/java/com/example/student_service/StudentServiceApplication.java
+++ src/main/java/com/example/student_service/StudentServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.student_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudentServiceApplication {
 
     public static void main(String[] args) {
